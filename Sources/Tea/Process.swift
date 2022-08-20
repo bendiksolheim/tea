dc18adc3ef02b97b413bf621241dfd0114f91ea6
Bendik Solheim <hello@bendik.dev>
2022-08-20 23:35:47 +0200

    Move cursor handling to Cursor struct

--- Sources/Tea/Process.swift
+++ Sources/Tea/Process.swift
@@ -8,14 +8,6 @@ public class Tea {
     public static func quit<Msg>(_ msg: String? = nil) -> Cmd<Msg> {
         Cmd(.Quit(.Success(msg)))
     }
-    
-    public static func moveCursor<Msg>(_ dx: XDelta, _ dy: YDelta) -> Cmd<Msg> {
-        Cmd(.Terminal(.MoveCursor(dx, dy)))
-    }
-
-    public static func putCursor<Msg>(_ x: Int, _ y: Int) -> Cmd<Msg> {
-        Cmd(.Terminal(.PutCursor(x, y)))
-    }
 
     public static func sleep(_ interval: TimeInterval) -> Effect<Void> {
         let cappedInterval = max(interval, 0.0)
--- Sources/Tea/View/Cursor.swift
+++ Sources/Tea/View/Cursor.swift
@@ -11,4 +11,16 @@ public struct Cursor: Equatable, Encodable {
     func with(y: Int? = nil, scroll: Int? = nil) -> Cursor {
         Self(y: y ?? self.y, scroll: scroll ?? self.scroll)
     }
+
+    public static func move<Msg>(_ dx: Int, _ dy: Int) -> Cmd<Msg> {
+        Cmd(.Terminal(.MoveCursor(dx, dy)))
+    }
+
+    public static func put<Msg>(_ x: Int, _ y: Int) -> Cmd<Msg> {
+        Cmd(.Terminal(.PutCursor(x, y)))
+    }
+
+    public static func scroll<Msg>(_ x: Unit) -> Cmd<Msg> {
+        Cmd(.Terminal(.Scroll(x)))
+    }
 }
